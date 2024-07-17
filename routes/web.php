e51b8fe585eac7a6a43edd7b237e29f3d8810a28
DaniloHernandez25 <dahernandez7@espe.edu.ec>
2024-07-17 10:14:22 -0500

    Agregados archivos de Livewire para CartPage, CategoriesPage, ProductDetailPage, ProductsPage y vistas asociadas. Modificaciones en home-page y navbar. Actualización de rutas en web.php y ajustes en vite.config.js.

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,30 @@
 
 use Illuminate\Support\Facades\Route;
 use Livewire\Livewire;
-use Filament\Http\Middleware\Authenticate; // Importa el middleware de autenticación de Filament
+use App\Http\Controllers\HomeController;
 use App\Livewire\HomePage;
-
+use App\Livewire\CategoriesPage;
+use App\Livewire\ProductsPage;
+use App\Livewire\CartPage;
+use App\Livewire\ProductDetailPage;
 
 Route::get('/', HomePage::class);
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
 Livewire::setUpdateRoute(function($handle) {
     return Route::post('/uniformes/public/livewire/update', $handle);
 });
+
+// Ruta a CategoriesPage
+Route::get('/categories', CategoriesPage::class)->name('categories');
+
+// Ruta a ProductsPage
+Route::get('/products', ProductsPage::class)->name('products');
+
+// Ruta a CartPage
+Route::get('/cart', CartPage::class)->name('cart');
+
+// Ruta a ProductDetailPage
+Route::get('/products/{product}', ProductDetailPage::class)->name('product.detail');
