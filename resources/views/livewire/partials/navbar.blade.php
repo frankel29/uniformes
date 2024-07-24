18f725b3f46c1d3a0e1e72cf00a1b6e0dc182aef
frankel29 <137657714+frankel29@users.noreply.github.com>
2024-07-24 11:59:12 -0500

    home y colegios

--- resources/views/livewire/partials/navbar.blade.php
+++ resources/views/livewire/partials/navbar.blade.php
@@ -24,7 +24,7 @@
 
           <a wire:navigate class="font-medium {{request()->is('/')?'text-blue-600': 'text-gray-500'}} py-3 md:py-6 dark:text-blue-500 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="{{ url('/') }}" aria-current="page">Home</a>
 
-            <a wire:navigate class="font-medium {{request()->is('categories')?'text-blue-600': 'text-gray-500'}} hover:text-gray-400 py-3 md:py-6 dark:text-gray-400 dark:hover:text-gray-500 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="{{ route('categories') }}">
+            <a wire:navigate class="font-medium {{request()->is('/categories')?'text-blue-600': 'text-gray-500'}} hover:text-gray-400 py-3 md:py-6 dark:text-gray-400 dark:hover:text-gray-500 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="{{ route('categories') }}">
               Categorias
             </a>
 
