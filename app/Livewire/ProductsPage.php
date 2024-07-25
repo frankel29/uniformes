5168969fa4f354a26a3b8d64b44b7cade0fdf03f
DaniloHernandez25 <dahernandez7@espe.edu.ec>
2024-07-24 21:14:16 -0500

    cambios en los productos

--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -2,12 +2,20 @@
 
 namespace App\Livewire;
 
+use App\Models\Product;
+use Livewire\Attributes\title;
 use Livewire\Component;
+use Livewire\WithPagination;
 
+#[Title('Products - RMStrudio')]
 class ProductsPage extends Component
 {
+    use WithPagination;
     public function render()
     {
-        return view('livewire.products-page');
+        $productQuery = Product::query()->where('is_active', 1);
+        return view('livewire.products-page', [
+            'products' => $productQuery->paginate(6),
+        ]);
     }
 }
