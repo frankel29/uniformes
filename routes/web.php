7f416cc3e7a560aa7c6cdb6eb518c46aca6cdfec
DaniloHernandez25 <dahernandez7@espe.edu.ec>
2024-07-24 21:20:38 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,5 @@
 <?php
 
-use App\Livewire\Auth\ResetPasswordPage;
-use App\Livewire\CancelPage;
-use App\Livewire\SuccessPage;
 use Illuminate\Support\Facades\Route;
 use Livewire\Livewire;
 use App\Http\Controllers\HomeController;
@@ -17,6 +14,9 @@ use App\Livewire\MyOrdersDetailPage;
 use App\Livewire\Auth\LoginPage;
 use App\Livewire\Auth\RegisterPage;
 use App\Livewire\Auth\ForgotPasswordPage;
+use App\Livewire\Auth\ResetPasswordPage;
+use App\Livewire\SuccessPage;
+use App\Livewire\CancelPage;
 
 Route::get('/', HomePage::class);
 Auth::routes();
@@ -37,7 +37,7 @@ Route::get('/products', ProductsPage::class)->name('products');
 Route::get('/cart', CartPage::class)->name('cart');
 
 // Ruta a ProductDetailPage
-Route::get('/products/{product}', ProductDetailPage::class)->name('products.show');
+Route::get('/products/{product}', ProductDetailPage::class)->name('product.detail');
 
 // Ruta a CheckoutPage
 Route::get('/checkout', CheckoutPage::class)->name('checkout');
@@ -52,12 +52,13 @@ Route::get('/login', LoginPage::class)->name('login');
 
 Route::get('/register', RegisterPage::class)->name('register');
 
-Route::get('/forgot', ForgotPasswordPage::class)->name('forgot');
+
+Route::get('/reset', ResetPasswordPage::class)->name('password.reset');
+
+Route::get('/forgot', ForgotPasswordPage::class);
 
 
-Route::get('/reset', ResetPasswordPage::class)->name('reset');
+Route::get('/success', SuccessPage::class);
 
-Route::get('/succes', SuccessPage::class)->name('succes');
 
-Route::get('/cancel', CancelPage::class)->name('cancel');
-
+Route::get('/cancel', CancelPage::class);
