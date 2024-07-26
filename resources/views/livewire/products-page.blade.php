7d0eb32755aad9f29308ee5d354819236b66e7aa
DaniloHernandez25 <dahernandez7@espe.edu.ec>
2024-07-26 11:43:53 -0500

    Eliminación de Prices

--- resources/views/livewire/products-page.blade.php
+++ resources/views/livewire/products-page.blade.php
@@ -38,18 +38,6 @@
               </li>
             </ul>
           </div>
-
-          <div class="p-4 mb-5 bg-white border border-gray-200 dark:bg-gray-900 dark:border-gray-900">
-            <h2 class="text-2xl font-bold dark:text-gray-400">Price</h2>
-            <div class="w-16 pb-2 mb-6 border-b border-rose-600 dark:border-gray-400"></div>
-            <div>
-              <input type="range" class="w-full h-1 mb-4 bg-blue-100 rounded appearance-none cursor-pointer" max="500000" value="100000" step="100000">
-              <div class="flex justify-between ">
-                <span class="inline-block text-lg font-bold text-blue-400 ">&#8377; 1000</span>
-                <span class="inline-block text-lg font-bold text-blue-400 ">&#8377; 500000</span>
-              </div>
-            </div>
-          </div>
         </div>
         <div class="w-full px-3 lg:w-3/4">
           <div class="px-3 mb-4">
