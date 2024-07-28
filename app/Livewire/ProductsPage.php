5e3784c408f2b5a5fdf6aac548ab68180b6a98cd
fernandom02 <famoya6@espe.edu.ec>
2024-07-28 10:09:24 -0500

    Moya15

--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -5,6 +5,7 @@ namespace App\Livewire;
 use App\Models\Product;
 use App\Models\Category;
 use Livewire\Attributes\Title;
+use Livewire\Attributes\Url;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -13,12 +14,33 @@ class ProductsPage extends Component
 {
     use WithPagination;
 
+    #[Url]
+    public $featured;
+
+    #[Url]
+    public $on_sale;
+
+    #[Url]
+    public $selected_categories = [];
+
     public function render()
     {
         $productQuery = Product::query()->where('is_active', 1);
 
+        if(!empty($this->selected_categories)) {
+          $productQuery->whereIn('category_id', $this->selected_categories);    
+        }
+
+        if($this->featured){
+          $productQuery->where('is_featured', 1);  
+        }
+
+        if($this->on_sale){
+            $productQuery->where('on_sale', 1);  
+          } 
+
         return view('livewire.products-page', [
-            'products' => $productQuery->paginate(6),
+            'products' => $productQuery->paginate(9),
             'categories' => Category::where('is_active', 1)->get(['id', 'name', 'slug']),
         ]);
     }
